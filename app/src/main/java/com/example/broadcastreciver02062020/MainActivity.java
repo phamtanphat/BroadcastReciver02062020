b0e52a3ce08d9255c2f0d20fb11ddc7ef1909efd
phatdroid94 <phatdroid94@gmail.com>
2020-07-25 18:00:58 +0700

    broadcast

--- app/src/main/java/com/example/broadcastreciver02062020/MainActivity.java
+++ app/src/main/java/com/example/broadcastreciver02062020/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.broadcastreciver02062020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    MyBroadCastReciever myBroadCastReciever = new MyBroadCastReciever();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        intentFilter.addAction("android.net.wifi.WIFI_STATE_CHANGED");
+        registerReceiver(myBroadCastReciever,intentFilter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(myBroadCastReciever);
     }
 }
--- app/src/main/java/com/example/broadcastreciver02062020/MyBroadCastReciever.java
+++ app/src/main/java/com/example/broadcastreciver02062020/MyBroadCastReciever.java
@@ -4,10 +4,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
-import android.util.Log;
 import android.widget.Toast;
 
-public class MyServiceBackground extends BroadcastReceiver {
+public class MyBroadCastReciever extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         ConnectivityManager connMgr = (ConnectivityManager) context
